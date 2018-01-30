d85ea3e62f21f8f95ab8657148cac1bd3590b5af
Alexander Larios <alarios91@gmail.com>
2018-01-30 17:16:52 -0600

    counters and text views
    Added counters and text views and assigned the counter value to be desplayed on it's text view

--- UmpireBuddy/app/src/main/java/alexlarios/umpirebuddy/MainActivity.java
+++ UmpireBuddy/app/src/main/java/alexlarios/umpirebuddy/MainActivity.java
@@ -1,5 +1,6 @@
 package alexlarios.umpirebuddy;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,20 +9,30 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button strikeButton, ballButton;
-    private TextView ballNumber, strikeNumber;
+    private TextView ballTextView, strikeTextView;
+    private int ballCount = 0, strikeCount= 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-     strikeButton = (Button) findViewById(R.id.strikeButton)
-
-
+     strikeButton = (Button) findViewById(R.id.strikeButton);
+     ballButton = (Button) findViewById(R.id.ballButton);
+     ballTextView = (TextView) findViewById(R.id.ballTextView);
+     strikeTextView = (TextView) findViewById(R.id.strikeTextView);
     }
 
+
     public void onStrikeButton(View view) {
+        strikeCount++;
+        strikeTextView.setText(String.valueOf(strikeCount));
+        if(strikeCount == 3){
+
+        }
     }
 
     public void onBallButton(View view) {
+        ballCount++;
+        ballTextView.setText(String.valueOf(ballCount));
     }
 }
